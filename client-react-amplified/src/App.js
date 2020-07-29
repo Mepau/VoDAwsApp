ed8f39582eaac9b79bc669261af7fd6ec003d085
Michael Perez <michaelperezabreu@gmail.com>
2020-07-29 13:12:34 -0400

    feat: cognito auth services

--- client-react-amplified/src/App.js
+++ client-react-amplified/src/App.js
@@ -1,26 +1,69 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+/* src/App.js */
+import React, { useEffect, useState } from 'react'
+import Amplify, { API, graphqlOperation } from 'aws-amplify'
+import { createVideo } from './graphql/mutations'
+import { listVideos } from './graphql/queries'
+import { withAuthenticator } from '@aws-amplify/ui-react'
 
-function App() {
+import awsExports from "./aws-exports";
+Amplify.configure(awsExports);
+
+const initialState = { name: '', description: '' }
+
+const App = () => {
+  const [formState, setFormState] = useState(initialState)
+  const [videos, setVideos] = useState([])
+
+  useEffect(() => {
+    fetchVideos()
+  }, [])
+
+  function setInput(key, value) {
+    setFormState({ ...formState, [key]: value })
+  }
+
+  async function fetchVideos() {
+    try {
+      const videoData = await API.graphql(graphqlOperation(listVideos))
+      const videos = videoData.data.listVideos.items
+      setVideos(videos)
+    } catch (err) { console.log('error fetching videos') }
+  }
+
+  async function addVideo() {
+    try {
+      if (!formState.name || !formState.description) return
+      const video = { ...formState }
+      setVideos([...videos, video])
+      setFormState(initialState)
+      await API.graphql(graphqlOperation(createVideo, {input: video}))
+    } catch (err) {
+      console.log('error creating video:', err)
+    }
+  }
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div style={styles.container}>
+      <h2>Amplify Videos</h2>
+      {
+        videos.map((video, index) => (
+          <div key={video.id ? video.id : index} style={styles.video}>
+            <p style={styles.videoName}>{video.name}</p>
+            <p style={styles.videoDescription}>{video.description}</p>
+          </div>
+        ))
+      }
     </div>
-  );
+  )
 }
 
-export default App;
+const styles = {
+  container: { width: 400, margin: '0 auto', display: 'flex', flex: 1, flexDirection: 'column', justifyContent: 'center', padding: 20 },
+  video: {  marginBottom: 15 },
+  input: { border: 'none', backgroundColor: '#ddd', marginBottom: 10, padding: 8, fontSize: 18 },
+  videoName: { fontSize: 20, fontWeight: 'bold' },
+  videoDescription: { marginBottom: 0 },
+  button: { backgroundColor: 'black', color: 'white', outline: 'none', fontSize: 18, padding: '12px 0px' }
+}
+
+export default withAuthenticator(App);
