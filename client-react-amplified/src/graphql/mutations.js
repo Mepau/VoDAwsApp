529aeada5633aca593567e50651a6321fae29df7
Michael Perez <michaelperezabreu@gmail.com>
2020-07-29 12:41:26 -0400

    Added graphql schema from AWS AppSync

---
+++ client-react-amplified/src/graphql/mutations.js
@@ -0,0 +1,42 @@
+/* eslint-disable */
+// this is an auto generated file. This will be overwritten
+
+export const createVideo = /* GraphQL */ `
+  mutation CreateVideo($input: CreateVideoInput!) {
+    createVideo(input: $input) {
+      guid
+      srcHeight
+      srcWidth
+      hlsUrl
+      mp4Urls
+      srcMediainfo
+      srcVideo
+    }
+  }
+`;
+export const updateVideo = /* GraphQL */ `
+  mutation UpdateVideo($input: UpdateVideoInput!) {
+    updateVideo(input: $input) {
+      guid
+      srcHeight
+      srcWidth
+      hlsUrl
+      mp4Urls
+      srcMediainfo
+      srcVideo
+    }
+  }
+`;
+export const deleteVideo = /* GraphQL */ `
+  mutation DeleteVideo($input: DeleteVideoInput!) {
+    deleteVideo(input: $input) {
+      guid
+      srcHeight
+      srcWidth
+      hlsUrl
+      mp4Urls
+      srcMediainfo
+      srcVideo
+    }
+  }
+`;
