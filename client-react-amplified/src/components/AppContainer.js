1c002c8a2d910fd0425d7737c7a9ccc0c1929168
enmanuelcruzdejesus <enmanuelcruzdejesus@gmail.com>
2020-08-09 10:22:47 -0400

    adding nav

--- client-react-amplified/src/components/AppContainer.js
+++ client-react-amplified/src/components/AppContainer.js
@@ -5,7 +5,9 @@ import { listVideos } from '../graphql/queries';
 import VideoGrid from './VideoGrid';
 import UploadForm from './UploadForm';
 import { Grid, Button, Container, Box } from "@material-ui/core";
-
+import  'bootstrap/dist/css/bootstrap.min.css';
+import { Navbar, Nav, NavDropdown } from 'react-bootstrap';
+import './appContainerStyles.css';
 async function signOut() {
   try {
       await Auth.signOut();
@@ -22,14 +24,25 @@ const AppContainer = () => {
     fetchVideos()
   }, [])
 
+  const styles= {
+    marginTop: '50px'
+  }
+  const leftMenu = {
+    
+  }
+
   async function fetchVideos() {
       
     //GraphQL reports both fetches data anad error if any for each element
       try
       {
-        //If there are no errors set whole data
-        const videodata = await API.graphql(graphqlOperation(listVideos)) 
-        const videos = videodata.data.listVideos.items;
+        // If there are no errors set whole data
+        // const videodata = await API.graphql(graphqlOperation(listVideos)) 
+        // const videos = videodata.data.listVideos.items;
+        const videos = 
+        [
+          
+        ]
         setVideos(videos);
       } catch (e) {
         //if error set gathered data
@@ -40,28 +53,50 @@ const AppContainer = () => {
 
   return (
     <div>
-      <Box >     
-        <Button variant="contained" color="secondary" onClick= {() => signOut()}>
-          Sign Out
-        </Button>
-      </Box>
-      <Container display="flex">
-        <Grid container 
-              justify="center"
-              alignItems="center"
-              direction="row"
-              spacing={2}
-              maxWidth="md">
-          <Grid item >
-            <VideoGrid videos={videos}/>   
-          </Grid>
-          <Grid item>
-            <Box>
-              <UploadForm/>
-            </Box> 
-          </Grid>
-        </Grid>
-      </Container>
+      <div className= "container">
+        <Navbar className="navbar navbar-expand-lg navbar-dark fixed-top bg-dark">
+          <a class="navbar-brand" >AwsVideos</a>
+          <Button className="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarNav" aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">
+            <span className="navbar-toggler-icon"></span>
+          </Button>
+          <div className="collapse navbar-collapse justify-content-end" id="navbarNav">
+          <ul className="navbar-nav">
+            <li className="nav-item active">
+              <a className="nav-link" >Home <span className="sr-only">(current)</span></a>
+            </li>
+            <li className="nav-item" >
+              <a className="nav-link" >Sign Out</a>
+            </li>
+          
+          </ul>
+         </div>
+        </Navbar>  
+      </div>
+    <div style ={styles}>
+          <Box >     
+                <Button variant="contained" color="secondary" onClick= {() => signOut()}>
+                  Sign Out
+                </Button>
+              </Box>
+              <Container display="flex">
+                <Grid container 
+                      justify="center"
+                      alignItems="center"
+                      direction="row"
+                      spacing={2}
+                      maxWidth="md">
+                  <Grid item >
+                    <VideoGrid videos={videos}/>   
+                  </Grid>
+                  <Grid item>
+                    <Box>
+                      <UploadForm/>
+                    </Box> 
+                  </Grid>
+                </Grid>
+              </Container>
+          </div>
+            
     </div>
   );
   
