32b9fe818b3edebbff46c3665f7945240da5d89e
Michael Perez <michaelperezabreu@gmail.com>
2020-08-10 07:37:26 -0400

    feat: added capability to delete old non processed videos

--- client-react-amplified/src/components/ProcsList.js
+++ client-react-amplified/src/components/ProcsList.js
@@ -1,19 +1,48 @@
 /* src/App.js */
 import React, { useState } from 'react';
-import { Box, Container, List, ListItem, ListItemIcon, ListItemText    } from '@material-ui/core';
+import { Box, Container, List, ListItem, ListItemIcon, ListItemText, IconButton     } from '@material-ui/core';
+import DeleteIcon from '@material-ui/icons/Delete';
+import Auth from '@aws-amplify/auth';
+import Lambda from 'aws-sdk/clients/lambda'; // npm install aws-sdk
 
 const secondaryMsg = (video) => {
 return <Box>
           <div>
-              {video.workflowStatus + "ing"}
+              {"State: " + video.workflowStatus }
           </div>
           <div>
               {"Height resolution: " + video.srcHeight}
+          </div>
+          <div>
+              {"Width resolution: " + video.srcWidth}
           </div>
        </Box>
 }
 
 const ProcsList = (props) => {
+
+  
+
+  //Handler for obtaining pre signed S3 URL for uploads
+  const handleDeleteClick = (e, guid) => {
+
+    //Obtain auth credentials from aws sdk env
+    Auth.currentCredentials()
+    .then(credentials => {
+        const lambda = new Lambda({
+        credentials: Auth.essentialCredentials(credentials)
+    }); 
+    //Invoke lambda using arn and cognito users policies
+    // Send insdert form data as input for customizing file upload
+    lambda.invoke({
+        FunctionName: '746016492294:function:delete-dynamo-and-s3-object',
+        Payload: JSON.stringify({guid})
+    }, (err, data) => {
+        if (err) console.log(JSON.stringify(err)); // an error occurred
+        else      // successful response
+            console.log(JSON.parse(data.Payload));
+    });
+})}
  
   return (
     <Box display="flex"
@@ -34,6 +63,9 @@ const ProcsList = (props) => {
                     secondary={secondaryMsg(video)}
 
                   />
+              <IconButton onClick={(e) => { handleDeleteClick(e,video.guid)}} aria-label="delete">
+                <DeleteIcon />
+              </IconButton>
             </ListItem>:null
           ))
         }
